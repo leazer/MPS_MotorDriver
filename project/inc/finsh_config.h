e5e390b111bc0fb1a0d47530120b2461f3d5c8ba
leazer <zealerlu@foxmail.com>
2026-06-22 11:17:40 +0800

    feat(finsh): enable RT_USING_FINSH+CONSOLE; add finsh sources to CMake and Keil

--- project/inc/finsh_config.h
+++ project/inc/finsh_config.h
@@ -4,7 +4,7 @@
 #define __MSH_CFG_H__
 
 // <<< Use Configuration Wizard in Context Menu >>>
-//#define RT_USING_FINSH
+#define RT_USING_FINSH
 #define FINSH_USING_MSH
 #define FINSH_USING_MSH_ONLY
 // <h>FinSH Configuration
